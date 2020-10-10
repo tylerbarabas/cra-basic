1aaf7e3c3c64fb7306b217355da403533bbbec8c
Tyler Barabas <tylerbarabas@gmail.com>
2020-10-09 19:30:59 -0500

    background sequence

--- src/components/background-video/index.js
+++ src/components/background-video/index.js
@@ -1,20 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './index.scss'
 
 const BackgroundVideo = () => {
+  const [isHueRotate, setIsHueRotate] = useState(false)
+  const hue = isHueRotate ? 'hue-rotate' : ''
   return (
     <video
-      className="background-video"
+      className={`background-video ${hue}`}
       src="./video1.mp4"
       muted
       onLoadedData={e=>{
         const video = e.target
-        video.playbackRate = 1.5;
+        video.playbackRate = 2;
         video.play()
         const interval = setInterval(()=>{
+          if (video.currentTime > 3) video.playbackRate = Math.max((video.playbackRate * 0.92).toFixed(1), 0.2)
           if (video.currentTime > 5) {
             video.pause()
             clearInterval(interval)
+            setIsHueRotate(true)
           }
         }, 100)
       }}

a93a859af50dd4513a4e4cf8429c4ed2e8b74d88
Tyler Barabas <tylerbarabas@gmail.com>
2020-10-18 19:13:35 -0500

    blog is mocked up

--- src/components/blog/index.js
+++ src/components/blog/index.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { MiscContext } from '../../contexts/misc'
+import './index.scss'
 
 const Blog = () => {
+  const [ isIntroFinished ] = useContext(MiscContext).isIntroFinished
+  const display = isIntroFinished ? '' : 'none'
   return (
-    <div className="page blog">
-      <h1>Blog</h1>
+    <div className="page blog animate__animated animate__fadeIn" style={{display}}>
+      <div className="paper">
+        <img src="notebook-paper.png" className="paper-img" />
+        <div className="paper-inner">
+          <h1>And so I packed my shit and left...</h1>
+          <p>Shes a child of satan. And theres no escapin. Once she put her little voice inside my head.Shes a child of satan. And theres no escapin. Once she put her little voice inside my head.Shes a child of satan. And theres no escapin. Once she put her little voice inside my head.Shes a child of satan. And theres no escapin. Once she put her little voice inside my head.Shes a child of satan. And theres no escapin. Once she put her little voice inside my head.Shes a child of satan. And theres no escapin. Once she put her little voice inside my head.Shes a child of satan. And theres no escapin. Once she put her little voice inside my head.Shes a child of satan. And theres no escapin. Once she put her little voice inside my head.</p>
+        </div>
+      </div>
     </div>
   )
 }

7422e2a81da740c8f41de71d29e47da060a7479d
Tyler Barabas <tylerbarabas@gmail.com>
2020-10-20 01:45:28 -0500

    slap a photo on the epk page

--- src/components/electronic-press-kit/index.js
+++ src/components/electronic-press-kit/index.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { MiscContext } from '../../contexts/misc'
 
 const ElectronicPressKit = () => {
+  const [ isIntroFinished ] = useContext(MiscContext).isIntroFinished
+  const display = isIntroFinished ? '' : 'none'
   return (
-    <div className="page electronic-press-kit">
-      <h1>EPK</h1>
+    <div className="page electronic-press-kit animate__animated animate__fadeIn" style={{display}}>
+      <img src="terlingua.jpeg" style={{height: '50vh'}} />
     </div>
   )
 }

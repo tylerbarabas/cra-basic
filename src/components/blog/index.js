e443745df0808f37ff5180eb78b2a74ebdbfde25
Tyler Barabas <tylerbarabas@gmail.com>
2020-10-19 22:32:20 -0500

    new look for blog

--- src/components/blog/index.js
+++ src/components/blog/index.js
@@ -6,12 +6,10 @@ import './index.scss'
 const Blog = () => {
   const [ isIntroFinished ] = useContext(MiscContext).isIntroFinished
   const blog = useContext(BlogContext)
-  console.log('blog', blog)
   const display = isIntroFinished ? '' : 'none'
   return (
     <div className="page blog animate__animated animate__fadeIn animate__slow" style={{display}}>
       <div className="paper">
-        <img src="notebook-paper.png" className="paper-img" />
         <div className="paper-inner">
           <h1>And so I packed my shit and left...</h1>
           <p>Shes a child of satan. And theres no escapin. Once she put her little voice inside my head.Shes a child of satan. And theres no escapin. Once she put her little voice inside my head.Shes a child of satan. And theres no escapin. Once she put her little voice inside my head.Shes a child of satan. And theres no escapin. Once she put her little voice inside my head.Shes a child of satan. And theres no escapin. Once she put her little voice inside my head.Shes a child of satan. And theres no escapin. Once she put her little voice inside my head.Shes a child of satan. And theres no escapin. Once she put her little voice inside my head.Shes a child of satan. And theres no escapin. Once she put her little voice inside my head.</p>

a261b4ec188ace01e0d906a7268e95d6450e1414
Tyler Barabas <tylerbarabas@gmail.com>
2021-08-19 18:09:26 -0500

    back to basics

--- src/App.js
+++ src/App.js
@@ -1,51 +1,10 @@
 import React from 'react';
-import { Switch, Route, BrowserRouter, Link } from 'react-router-dom'
-import MiscProvider from './contexts/misc'
-import BackdropProvider from './contexts/backdrop'
-import BackgroundVideo from './components/background-video'
-import BlogProvider from './contexts/blog'
-import Logo from './components/logo'
-import Nav from './components/nav'
-import CallToAction from './components/call-to-action'
-import ElectronicPressKit from './components/electronic-press-kit'
-import Blog from './components/blog'
-import 'animate.css'
-import './App.css'
 
 function App() {
   return (
-    <MiscProvider>
-      <BlogProvider>
-        <div className="App">
-          <BrowserRouter>
-            <BackdropProvider>
-              <BackgroundVideo />
-              <Switch>
-                <Route
-                  exact
-                  path="/"
-                  component={CallToAction}
-                />
-                <Route
-                  exact
-                  path="/epk"
-                  component={ElectronicPressKit}
-                />
-                <Route
-                  exact
-                  path="/blog"
-                  component={Blog}
-                />
-              </Switch>
-              <Link to="/">
-                <Logo />
-              </Link>
-              <Nav />
-            </BackdropProvider>
-          </BrowserRouter>
-        </div>
-      </BlogProvider>
-    </MiscProvider>
+    <>
+      Hello world
+    </>
   );
 }
 
